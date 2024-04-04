3c108340bf3d8e01b099c30d3652eabc5be664c8
EVOAUCTION <125301054+JNDUNLAP@users.noreply.github.com>
2024-04-03 21:51:33 -0400

    r

--- gen/main.py
+++ gen/main.py
@@ -1,13 +1,13 @@
-from requirements_generator.create.xsd_processor import process_xsd_files
-from requirements_generator.create.go_struct_generator import generate_go_struct
+from gen.create.xsd_processor import process_xsd_files
+from gen.create.go_struct_generator import generate_go_struct
 import json
 import shutil
 import os
 
 
-json_output_file = 'requirements/element_details.json'
+json_output_file = 'req/element_details.json'
 golang_output_file = 'report_struct.go'
-xsd_files = ['requirements/QReport_17.03.xsd', 'requirements/stypes_17.03.xsd']
+xsd_files = ['req/QReport_17.03.xsd', 'req/stypes_17.03.xsd']
 target_path = '../app/model/report_struct.go'
 target_dir = os.path.dirname(target_path)
 
